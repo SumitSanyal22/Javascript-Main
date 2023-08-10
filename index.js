50e6da6b976e22ff92a2426db2121aaa8595be16
SumitSanyal22 <sumitsanyal671@gmail.com>
2023-08-10 00:47:10 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let age = 21;  // Int/ Number 
+
+age = age + 1
+
+console.log("You are Noe", age, "years old");
+
+
+let fristName = "Bro";   // string 
+let student = true   // booleans 
+
+
+console.log("Hello younger", fristName);
+console.log("Enrolled ", student);
